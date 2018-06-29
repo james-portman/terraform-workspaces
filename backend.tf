264b02e0fe87c76355ce449fbedccd53cfb0a1b5
James Portman <james.portman@coopdigital.co.uk>
2018-06-29 09:41:18 +0100

    .

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 # terraform {
 #   backend "s3" {
-#     bucket               = "coop-dpm-terraform-remote"
+#     bucket               = "xxxx-dpm-terraform-remote"
 #     region               = "eu-west-1"
 #     acl                  = "bucket-owner-full-control"
 #     workspace_key_prefix = "membership/service/sfg"
